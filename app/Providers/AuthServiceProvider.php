c2eda97f6fb9568fd78751b5e55231631fac3082
Sorethea <sorethea@gmail.com>
2022-08-17 16:04:21 +0700

    First commit

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Sorethea\FilakubeAdmin\Policies\UserPolicy;
 
 class AuthServiceProvider extends ServiceProvider
 {
